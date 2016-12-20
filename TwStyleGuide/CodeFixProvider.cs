dff0e54ac0503f377988df0c4b8500f93523815a
rkblackfire <krug@tecware-gmbh.de>
2016-12-20 11:54:31 +0100

    initial commit

---
+++ TwStyleGuide/CodeFixProvider.cs
@@ -0,0 +1,193 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+
+namespace TwStyleGuide
+{
+	/// <summary>
+	/// IMPORTANT!: Only the shown functions should be in here.
+	/// other functions (''public void Herbert() { // I like Mondays }'') will block every CodeFixProvider in here! Check that, if something doesn't work!
+	/// </summary>
+	[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(StatementIndentCodeFixProvider)), Shared]
+	public class StatementIndentCodeFixProvider : CodeFixProvider
+	{
+		/// <summary>
+		/// This array publishes the DiagnosticIds, this CodeFixProvider can Fix. 
+		/// </summary>
+		public sealed override ImmutableArray<string> FixableDiagnosticIds
+		{
+			get { return ImmutableArray.Create("TW0001"); }
+		}
+
+		/// <summary>
+		/// This indicates, that the Fix can be run beside other Fixes (async)
+		/// </summary>
+		/// <returns>I don't care</returns>
+		public sealed override FixAllProvider GetFixAllProvider()
+		{
+			// See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
+			return WellKnownFixAllProviders.BatchFixer;
+		}
+
+		/// <summary>
+		/// This knits the CodeFix to the Function which actually does the Job
+		/// CodeAction.Create() can use createChangedDocument as Well as createChangedSolution(), indication if the whole solution will be changed by the Fix or only the actual Document.
+		/// Use this wisely because the performance will drop
+		/// In case of Solution the Fixwer have to return Task of Solution, otherwise Task of Document
+		/// </summary>
+		/// <param name="context">the context is the Location, reported by the Diagnostic Analyser, to knit the Fix-Function to that</param>
+		/// <returns></returns>
+		public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+		{
+			var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+			var diagnostic = context.Diagnostics.First();
+			var diagnosticSpan = diagnostic.Location.SourceSpan;
+			StatementSyntax statement = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<StatementSyntax>().First();
+			
+			if (statement != null) context.RegisterCodeFix(CodeAction.Create(title: "Place the statement on a new line.",
+																								  createChangedDocument: c => PlaceOnNewLine(context.Document, statement, c),
+																								  equivalenceKey: "Place the statement on a new line."),
+																		  diagnostic);
+		}
+
+		/// <summary>
+		/// This is the actual Fix
+		/// </summary>
+		/// <param name="document">the whole Document (Tree)</param>
+		/// <param name="ifStatement">the problem</param>
+		/// <param name="cancellationToken">used to check for that</param>
+		/// <returns>the Changes in the Syntaxtree</returns>
+		private Task<Document> PlaceOnNewLine(Document document, StatementSyntax statement, CancellationToken cancellationToken)
+		{
+			SyntaxNode oldRoot;
+			document.TryGetSyntaxRoot(out oldRoot);
+			
+			dynamic dynamicStatement = statement;
+
+			// the Replace*-function ensures that only the changes use-up memory, while the unchanged content is ref'd in memory (that's at least the idea)
+			// .WithAdditionalAnnotations(Formatter.Annotation) formats the indentation according to the settings of the editor (tabs or [1..n]whitespaces) - neat
+			var newRoot = oldRoot.ReplaceNode((SyntaxNode)dynamicStatement.Statement, ((StatementSyntax)dynamicStatement.Statement).WithLeadingTrivia(SyntaxFactory.LineFeed).WithAdditionalAnnotations(Formatter.Annotation));
+			var newDocument = document.WithSyntaxRoot(newRoot);
+			return Task.FromResult(newDocument);
+		}
+	}
+
+	[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(StringEmptyCodeFixProvider)), Shared]
+	public class StringEmptyCodeFixProvider : CodeFixProvider
+	{
+		public sealed override ImmutableArray<string> FixableDiagnosticIds
+		{
+			get { return ImmutableArray.Create("TW0002"); }
+		}
+
+		public sealed override FixAllProvider GetFixAllProvider()
+		{
+			// See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
+			return WellKnownFixAllProviders.BatchFixer;
+		}
+
+		public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+		{
+			var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+			var diagnostic = context.Diagnostics.First();
+			var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+			var stringInitViolation = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<LocalDeclarationStatementSyntax>().First();
+
+			//Register a code action that will invoke the fix.
+			context.RegisterCodeFix(
+				CodeAction.Create(
+					title: "Initialize the string with String.Empty.",
+					createChangedDocument: c => UseStringEmpty(context.Document, stringInitViolation, c),
+				equivalenceKey: "Initialize the string with String.Empty."),
+				context.Diagnostics);
+
+		}
+
+		private async Task<Document> UseStringEmpty(Document document, LocalDeclarationStatementSyntax stringInitViolation, CancellationToken cancellationToken)
+		{
+			var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
+
+			var oldInitializer = stringInitViolation.Declaration.Variables[0].Initializer;
+
+			ExpressionSyntax es = SyntaxFactory.ParseExpression("String.Empty").WithLeadingTrivia(SyntaxFactory.Space);
+			var newInitializer = SyntaxFactory.EqualsValueClause(es);
+
+			var root = await document.GetSyntaxRootAsync();
+
+			var newRoot = root.ReplaceNode(oldInitializer, newInitializer);
+
+			var newDocument = document.WithSyntaxRoot(newRoot);
+			return newDocument;
+		}
+	}
+
+	[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(CommentSingleSpaceCodeFixProvider)), Shared]
+	public class CommentSingleSpaceCodeFixProvider : CodeFixProvider
+	{
+		/// <summary>
+		/// This array publishes the DiagnosticIds, this CodeFixProvider can Fix. 
+		/// </summary>
+		public sealed override ImmutableArray<string> FixableDiagnosticIds
+		{
+			get { return ImmutableArray.Create("TW0003"); }
+		}
+
+		/// <summary>
+		/// This indicates, that the Fix can be run beside other Fixes (async)
+		/// </summary>
+		/// <returns>I don't care</returns>
+		public sealed override FixAllProvider GetFixAllProvider()
+		{
+			// See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
+			return WellKnownFixAllProviders.BatchFixer;
+		}
+
+		/// <summary>
+		/// This knits the CodeFix to the Function which actually does the Job
+		/// CodeAction.Create() can use createChangedDocument as Well as createChangedSolution(), indication if the whole solution will be changed by the Fix or only the actual Document.
+		/// Use this wisely because the performance will drop
+		/// In case of Solution the Fixwer have to return Task of Solution, otherwise Task of Document
+		/// </summary>
+		/// <param name="context">the context is the Location, reported by the Diagnostic Analyser, to knit the Fix-Function to that</param>
+		/// <returns></returns>
+		public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+		{
+			var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+			var diagnostic = context.Diagnostics.First();
+			var diagnosticSpan = diagnostic.Location.SourceSpan;
+			var comment = root.FindTrivia(diagnosticSpan.Start);
+
+			// Register a code action that will invoke the fix.
+			context.RegisterCodeFix(CodeAction.Create(title: "Insert the missing whitespace.",
+																	  createChangedDocument: c => InsertWhitespaceInComment(context.Document, comment, c),
+																	  equivalenceKey: "Insert the missing whitespace."),
+											 diagnostic);
+		}
+
+		private Task<Document> InsertWhitespaceInComment(Document document, SyntaxTrivia comment, CancellationToken c)
+		{
+			SyntaxNode oldRoot;
+			document.TryGetSyntaxRoot(out oldRoot);
+
+			var oldComment = comment.ToString();
+			var index = 0;
+			while (index < oldComment.Length && oldComment[index] == '/')
+				index++;
+
+			var spacedComment = SyntaxFactory.SyntaxTrivia(SyntaxKind.SingleLineCommentTrivia, oldComment.Insert(index, " ")); // this RegExp places the whitespace behind the last slash
+
+			var newRoot = oldRoot.ReplaceTrivia(comment, spacedComment);
+			var newDocument = document.WithSyntaxRoot(newRoot);
+			return Task.FromResult(newDocument);
+		}
+	}
+}
---
+++ TwStyleGuide/DiagnosticAnalyzer.cs
@@ -0,0 +1,193 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Text.RegularExpressions;
+using Microsoft.CodeAnalysis.Formatting;
+
+namespace TwStyleGuide
+{
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public class TwStyleGuideAnalyzer : DiagnosticAnalyzer
+	{
+		private static DiagnosticDescriptor Rule1 = new DiagnosticDescriptor("TW0001",
+																								  "Checks for LineBreak and Indentation",
+																								  "The Statement \"{0}\" must be placed in a new Line.",
+																								  "Layout",
+																								  DiagnosticSeverity.Warning,
+																								  isEnabledByDefault: true,
+																								  description: "A Statement should be on a new line and indented");  // DiagnosticSeverity.Error will not compile if existent
+		private static DiagnosticDescriptor Rule2 = new DiagnosticDescriptor("TW0002",
+																						  "Checks for string initialisation",
+																						  "The string \"{0}\" should not be inialized by \"{1}\" but with \"String.Empty\".",
+																						  "Variables",
+																						  DiagnosticSeverity.Warning, // Squiggles only on Warning and Error, Info has none
+																						  isEnabledByDefault: true);  // DiagnosticSeverity.Error will not compile if existent
+		private static DiagnosticDescriptor Rule3 = new DiagnosticDescriptor("TW0003",
+																						  "Checks for Comment formatting",
+																						  "There should be a whitespace between the slashes and the comment.",
+																						  "Layout",
+																						  DiagnosticSeverity.Warning, // Squiggles only on Warning and Error, Info has none
+																						  isEnabledByDefault: true);  // DiagnosticSeverity.Error will not compile if existent
+		private static DiagnosticDescriptor Rule4 = new DiagnosticDescriptor("TW0004",
+																						  "Checks for mandatory Comments",
+																						  "The public {1} \"{0}\" has no comment.",
+																						  "Layout",
+																						  DiagnosticSeverity.Warning, // Squiggles only on Warning and Error, Info has none
+																						  isEnabledByDefault: true);  // DiagnosticSeverity.Error will not compile if existent
+
+		/// <summary>
+		/// The array publishes the Check-Rules
+		/// </summary>
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule1, Rule2, Rule3, Rule4); } }
+
+		/// <summary>
+		/// In the Initialize Method one have to Register all Check-Functions
+		/// </summary>
+		/// <param name="context">The context (the Solution or Project)</param>
+		public override void Initialize(AnalysisContext context)
+		{
+			context.RegisterSyntaxNodeAction(AnalyzeIndentationOfSyntaxNode, SyntaxKind.IfStatement, SyntaxKind.ForStatement, SyntaxKind.WhileStatement, SyntaxKind.ForEachStatement);
+			context.RegisterSyntaxNodeAction(AnalyzeVariableDeclarationSyntaxNode, SyntaxKind.LocalDeclarationStatement);
+			context.RegisterSyntaxTreeAction(AnalyzeSingleLineCommentSyntaxNode);   // SyntaxKind.SingleLineComment does not work/is not fired
+			context.RegisterSyntaxNodeAction(AnalyzePublicMethodComment, SyntaxKind.MethodDeclaration);
+			context.RegisterSyntaxNodeAction(AnalyzePublicVarComment, SyntaxKind.FieldDeclaration);
+			context.RegisterSyntaxNodeAction(AnalyzePublicEnumComment, SyntaxKind.EnumDeclaration);
+			context.RegisterSyntaxNodeAction(AnalyzePublicStructComment, SyntaxKind.StructDeclaration);
+			context.RegisterSyntaxNodeAction(AnalyzePublicPropertyComment, SyntaxKind.PropertyDeclaration);
+		}
+
+		private void AnalyzePublicPropertyComment(SyntaxNodeAnalysisContext context)
+		{
+
+			if (!((PropertyDeclarationSyntax)context.Node).Modifiers.Any(SyntaxKind.PublicKeyword)) return;
+			var singleLineComments = from comment in context.Node.DescendantTrivia() where comment.IsKind(SyntaxKind.SingleLineDocumentationCommentTrivia) select comment;
+
+			if (singleLineComments.Count() == 0)
+			{
+				var diagnostic = Diagnostic.Create(Rule4,
+															  ((PropertyDeclarationSyntax)context.Node).Identifier.GetLocation(),
+															  ((PropertyDeclarationSyntax)context.Node).Identifier.ToString(),
+															  "Property");
+				context.ReportDiagnostic(diagnostic);
+			}
+		}
+
+		private void AnalyzePublicStructComment(SyntaxNodeAnalysisContext context)
+		{
+
+			if (!((StructDeclarationSyntax)context.Node).Modifiers.Any(SyntaxKind.PublicKeyword)) return;
+			var singleLineComments = from comment in context.Node.DescendantTrivia() where comment.IsKind(SyntaxKind.SingleLineDocumentationCommentTrivia) select comment;
+			if (singleLineComments.Count() == 0)
+			{
+
+				var diagnostic = Diagnostic.Create(Rule4,
+															  ((StructDeclarationSyntax)context.Node).Identifier.GetLocation(),
+															  ((StructDeclarationSyntax)context.Node).Identifier.ToString(),
+															  "struct");
+				context.ReportDiagnostic(diagnostic);
+			}
+		}
+
+		private void AnalyzePublicEnumComment(SyntaxNodeAnalysisContext context)
+		{
+
+			if (!((EnumDeclarationSyntax)context.Node).Modifiers.Any(SyntaxKind.PublicKeyword)) return;
+			var singleLineComments = from comment in context.Node.DescendantTrivia() where comment.IsKind(SyntaxKind.SingleLineDocumentationCommentTrivia) select comment;
+			if (singleLineComments.Count() == 0)
+				{
+
+					var diagnostic = Diagnostic.Create(Rule4, 
+																  ((EnumDeclarationSyntax)context.Node).Identifier.GetLocation(), 
+																  ((EnumDeclarationSyntax)context.Node).Identifier.ToString(), 
+																  "enum");
+					context.ReportDiagnostic(diagnostic);
+				}
+		}
+
+		private void AnalyzePublicVarComment(SyntaxNodeAnalysisContext context)
+		{
+
+			if (!((FieldDeclarationSyntax)context.Node).Modifiers.Any(SyntaxKind.PublicKeyword)) return;
+			var singleLineComments = from comment in context.Node.DescendantTrivia() where comment.IsKind(SyntaxKind.SingleLineDocumentationCommentTrivia) select comment;
+			if (singleLineComments.Count() == 0)
+				foreach (var variable in ((FieldDeclarationSyntax)context.Node).Declaration.Variables)
+				{
+				
+				var diagnostic = Diagnostic.Create(Rule4, 
+															  variable.Identifier.GetLocation(), 
+															  variable.Identifier.ToString(), 
+															  ((FieldDeclarationSyntax)context.Node).Declaration.Type.ToString());
+				context.ReportDiagnostic(diagnostic);
+			}
+		}
+
+		private void AnalyzePublicMethodComment(SyntaxNodeAnalysisContext context)
+		{
+			
+			if (!((MethodDeclarationSyntax)context.Node).Modifiers.Any(SyntaxKind.PublicKeyword)) return;
+			var singleLineComments = from comment in context.Node.DescendantTrivia() where comment.IsKind(SyntaxKind.SingleLineDocumentationCommentTrivia) select comment;
+			if (singleLineComments.Count() == 0)
+			{
+				var diagnostic = Diagnostic.Create(Rule4, 
+															  ((MethodDeclarationSyntax)context.Node).Identifier.GetLocation(), 
+															  ((MethodDeclarationSyntax)context.Node).Identifier.Value.ToString(), 
+															  "Method");
+				context.ReportDiagnostic(diagnostic);
+			}
+		}
+
+		private void AnalyzeSingleLineCommentSyntaxNode(SyntaxTreeAnalysisContext context)
+		{
+			SyntaxNode root = context.Tree.GetCompilationUnitRoot(context.CancellationToken);
+			var commentNodes = from node in root.DescendantTrivia() where node.IsKind(SyntaxKind.SingleLineCommentTrivia) select node;  //  one could also select ''node.IsKind(SyntaxKind.SingleLineDocumentationCommentTrivia)''
+			foreach (var node in commentNodes)
+				if (!Regex.IsMatch(node.ToString(), @"/{2,}\s"))
+				{
+					var diagnostic = Diagnostic.Create(Rule3, node.GetLocation());
+					context.ReportDiagnostic(diagnostic);
+				}
+		}
+
+		/// <summary>
+		/// This is a presentation on finding Statements which are in the same Line as the Condition
+		/// it is executed, when a Syntax Node is finished
+		/// These Analyze-functions can check for&throw multiple rule violations.
+		/// </summary>
+		/// <param name="context">The Context ist the Sourcecode</param>
+		private void AnalyzeIndentationOfSyntaxNode(SyntaxNodeAnalysisContext context)
+		{
+			dynamic syntobj = context.Node;
+
+			if (!syntobj.Statement.HasLeadingTrivia && !syntobj.Statement.IsMissing) // it's not an EoL-Trivia && there is a statement (you are not typing)
+			{
+				var messageHint = Regex.Replace(((string)syntobj.Statement.GetText().ToString()), @"\s+|\t|\n|\r", " "); // cleanup the Warningmessage
+				if (messageHint.Length > 30) messageHint = messageHint.Substring(0, 30) + "...";	// limit it to 30 chars
+				/// Diagnostic.Create(the rule violated, the Location() - for the squiggles, [0..n] parameters - passed to the ''Rule.MessageFormat'')
+				var diagnostic = Diagnostic.Create(Rule1, syntobj.GetLocation(), messageHint); 
+				context.ReportDiagnostic(diagnostic);
+			}
+		}
+
+		/// <summary>
+		/// This checks, if a String is initialized with "" rather than String.Empty (TW-CD)
+		/// it is executed, when a Syntax Node is finished
+		/// </summary>
+		/// <param name="context"></param>
+		private void AnalyzeVariableDeclarationSyntaxNode(SyntaxNodeAnalysisContext context)
+		{
+			var syntax = (LocalDeclarationStatementSyntax)context.Node;
+			if (context.SemanticModel.GetTypeInfo(syntax.Declaration.Type).ConvertedType.SpecialType == SpecialType.System_String)
+				foreach (var variable in syntax.Declaration.Variables) if (variable.Initializer?.Value.ToString() == "\"\"" || variable.Initializer?.Value.ToString() == "string.Empty")
+					{
+						var diagnostic = Diagnostic.Create(Rule2, variable.GetLocation(), variable.Identifier.Text, variable.Initializer.Value.ToString()); // the regex is to cleanup for the WarningMessage
+						context.ReportDiagnostic(diagnostic);
+					} 
+		}
+	}
+}
